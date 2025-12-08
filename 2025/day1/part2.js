0508288975158ed550927a565ebb553aa860ec0d
Tyler Kirkland <tyler.q.kirkland@gmail.com>
2025-12-08 11:23:25 -0800

    2025 day 1 part 2 scuffed

---
+++ 2025/day1/part2.js
@@ -0,0 +1,35 @@
+import * as utils from "../../utils/utils.js";
+
+const input = utils.getInput();
+
+let position = 50;
+let sum = 0;
+
+ for (let line of input) {
+  const direction = line.slice(0, 1);
+  const distance = line.slice(1);
+  const startingPosition = position;
+  position += ( direction === 'R' ) ? parseInt(distance) : -(distance);
+  
+  if (position < 0) {
+    sum += Math.ceil(Math.abs((position / 100)));
+    position = 100 - Math.abs(position % 100);
+    if ( startingPosition == 0 ) sum -= 1;
+  }
+
+  if ( position == 0 ) {
+    sum += 1;
+  }
+
+
+  if ( position > 99 ) {
+  sum += Math.abs(parseInt(position / 100, 10));
+  position = position % 100;
+  }  
+
+
+ }
+
+  console.log(position)
+
+utils.logOutput(2025, 1, "B", sum);
